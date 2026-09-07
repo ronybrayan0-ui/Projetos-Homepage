447188729a2fea8ea6fcae8a64748629b59ba0f0
Rony Brayan <ronybrayan0@gmail.com>
2026-09-07 18:33:39 -0300

    main Importações JS

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js - Ponto de entrada principal do JavaScript
+// Importa todos os módulos necessários para o projeto
+
+// Módulo do accordion (seção 1)
+import "./js/accordion.js";
+import "./js/menu-mobile.js";
+import "./js/secoes.js";
+import "./js/tabnav.js"
